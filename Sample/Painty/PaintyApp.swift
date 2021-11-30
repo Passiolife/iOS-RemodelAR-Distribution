9a0e215de847f7fd250780231ea77dbc8c378fab
Davido Hyer <davido@passiolife.com>
2021-11-30 08:12:30 -0700

    Added the sample app built in the documentation

---
+++ Sample/Painty/PaintyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaintyApp.swift
+//  Painty
+//
+//  Copyright © 2021 Passio Inc. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct PaintyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
